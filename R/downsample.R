4da51b03a3e905cf72833ee442ea054ab183072c
scottyler892 <scottyler89+bitbucket@gmail.com>
2022-01-12 19:55:39 -0500

    added retention of names and made parallel

--- R/downsample.R
+++ R/downsample.R
@@ -34,18 +34,70 @@ get_sim_transcripts<-function(in_col_vect, target_depth){
 #' are less than the cutoff, then set this value to the cutoff.
 #'
 #' @param in_mat An input matrix
-#' @param target_depth This can either be nothing or a 
-#' @return A matrix of the downsampled in_mat
+#' @param target_depth This can either be nothing or a specific target to 
+#' which we will normalize to. Note that this should be the minimum of colsum 
+#' across all datasets.
+#' @return A matrix of the normalized matrix
 #' @examples
 #'    in_mat<-matrix(rnbinom(1000*20000,.1,c(0.5,0.5,0.5)),ncol=1000,nrow=20000)
 #'    out_mat<-downsample_mat(in_mat)
+#' @importFrom parallel detectCores makeCluster parApply stopCluster
 #' @name downsample_mat
 #' @export
-downsample_mat<-function(in_mat, target_depth=NULL){
+downsample_mat<-function(in_mat, target_depth=NULL, rm_less=TRUE, parallel=TRUE, cores=NULL, quiet=FALSE){
   if (is.null(target_depth)){
     target_depth<-min(colSums(in_mat))
   }
-  out_mat <- apply(in_mat, 2, function(x) get_ds_transcript_vect(x, target_depth))
+  if (parallel){
+    if (is.null(cores)){
+        cores<- detectCores()
+    }
+    cl <- makeCluster(cores)
+    clusterExport(cl, c("get_ds_transcript_vect", "get_sim_transcripts"),
+              envir=environment())
+    if (!quiet){    
+      message("        running with ",cores," cores")
+    }
+    out_mat <- parApply(cl, X=in_mat, MARGIN=2, FUN=get_ds_transcript_vect, target_depth=target_depth)
+    stopCluster(cl)
+  } else {
+    out_mat <- apply(in_mat, 2, function(x) get_ds_transcript_vect(x, target_depth))
+  }
+  rownames(out_mat)<-rownames(in_mat)
+  colnames(out_mat)<-colnames(in_mat)
+  return(out_mat)
+}
+
+##############################################
+
+get_min_count_norm_per_col<-function(in_col_vect1, target_depth){
+  loading_factor<-sum(in_col_vect1)/target_depth
+  return(in_col_vect1/loading_factor)
+}
+
+
+#' Normalize columns to the min-count of a matrix
+#'
+#' This function takes in a matrix of counts & will 
+#'
+#' @param in_mat An input matrix
+#' @param target_depth This can either be nothing or a lower cutoff
+#' @param return_log If we should log2(n+1) transform after normalizing
+#' @param rm_less Will remove cells less than the cutoff rather than throwing an error.
+#' @return A matrix of the downsampled in_mat
+#' @examples
+#'    in_mat<-matrix(rnbinom(1000*20000,.1,c(0.5,0.5,0.5)),ncol=1000,nrow=20000)
+#'    out_mat<-min_count_norm_mat(in_mat)
+#' @name downsample_mat
+#' @export
+min_count_norm_mat<-function(in_mat, target_depth=NULL, return_log=FALSE, rm_less=TRUE){
+  if (is.null(target_depth)){
+    target_depth<-min(colSums(in_mat))
+  }
+  out_mat <- apply(in_mat, 2, function(x) get_min_count_norm_per_col(x, target_depth))
+  if (return_log){
+    out_mat<-log2(out_mat+1)
+  }
   return(out_mat)
 }
 
